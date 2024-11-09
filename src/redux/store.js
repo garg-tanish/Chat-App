bdfa1e75fe2828f9e7d31f9483ae72fa645f3177
Tanish Garg <tanishgarg596@gmail.com>
2024-11-09 21:31:29 +0530

    refactoring

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,8 @@
 import userReducer from './userSlice'
-import messageReducer from './messageSlice'
 import { configureStore } from '@reduxjs/toolkit'
-
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
-    messages: messageReducer
+    user: userReducer
   },
 })
